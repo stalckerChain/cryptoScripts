3353caa825c05bc0909541c7313a0cc0e85493ac
stalcker <stalcker@gmail.com>
2024-03-03 19:43:06 +0600

    init commit

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ import random
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(id_generator())
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+    # print(id_generator())
 
 
 
