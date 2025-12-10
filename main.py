ad46b61d689d9c7ff45a12326633c4dc8dc71124
stalckerChain <121632159+stalckerChain@users.noreply.github.com>
2025-12-10 20:05:51 +0600

    Update main.py

--- main.py
+++ main.py
@@ -42,4 +42,8 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('PyCharm')
 
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print_hi('PyCharm')
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
