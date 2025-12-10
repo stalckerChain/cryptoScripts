a507505f305b762ca7eb353a4f360a5693c17222
stalckerChain <121632159+stalckerChain@users.noreply.github.com>
2025-12-10 20:06:06 +0600

    Update main.py
    add function

--- main.py
+++ main.py
@@ -45,5 +45,7 @@ if __name__ == '__main__':
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('PyCharm')
-
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print_hi('PyCharm')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
