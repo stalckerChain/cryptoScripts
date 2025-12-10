394eae976126a9789f5d6f6ae1f393bee21ef765
stalckerChain <121632159+stalckerChain@users.noreply.github.com>
2025-12-10 20:05:27 +0600

    Update main.py
    more print 8

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.    
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.    
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.    
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.    
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.    
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.    
